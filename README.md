80d5a88700a80b49acf69fd3bc6b18c32786bda9
eunjee <rira9817@gmail.com>
2022-01-16 19:54:08 +0900

    파일업로드 실습

---
+++ README.md
@@ -0,0 +1,14 @@
+# Spring Cloud AWS와 AWS S3을 이용한 파일 업로드
+
+
+## 개발환경
+- Mac OS, Intellj IDEA 2021.1.3 (Ultimate)
+- JDK 11
+- Spring boot 2.5.3
+- Gradle 7.2 (Gradle Wrapper)
+- Spring Web MVC
+- Lombok
+
+
+블로그: 
+- [스프링 부트를 이용한 AWS S3 파일 업로드](https://willseungh0.tistory.com/2)
---
+++ src/main/java/com/beads/springawss3/external/UploadService.java
@@ -0,0 +1,13 @@
+package com.beads.springawss3.external;
+
+import com.amazonaws.services.s3.model.ObjectMetadata;
+
+import java.io.InputStream;
+
+public interface UploadService {
+
+	void uploadFile(InputStream inputStream, ObjectMetadata objectMetadata, String fileName);
+
+	String getFileUrl(String fileName);
+
+}
